85509bf0c958d3a3b7a41c66410b9d8c3d8b8df4
Den4ik-Bro <denis.rr@bk.ru>
2022-09-18 20:26:18 +0300

    change admin.py

--- notification_service/admin.py
+++ notification_service/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from . import models
 
 
-admin.site.register(models.User)
+admin.site.register(models.Client)
 admin.site.register(models.MailingList)
 admin.site.register(models.Message)
